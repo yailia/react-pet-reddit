08e0733a80841bea924ca617703c3295ca648fba
unknown <ilia@e1off.dev>
2022-04-27 13:16:16 +0300

    auth

---
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { EColor } from '../../../../enums/EColor';
+import { EIcon } from '../../../../enums/EIcon';
+import { Icon } from '../../../Icon';
+import { Text } from '../../../Text';
+import styles from './userblock.css';
+
+interface IUserBlockProps {
+  avatarSrc?: string;
+  username?: string;
+}
+
+export function UserBlock(props: IUserBlockProps) {
+  return (
+    <a
+      target="_blank"
+      href='https://www.reddit.com/api/v1/authorize?client_id=5iOVxSKsbB4vl3F-STR1xQ&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read identity submit'
+      className={styles.userBox
+    }>
+      <div className={styles.avatarBox}>
+        { 
+          props.avatarSrc ?
+            <img src={props.avatarSrc} alt="user avatar" className={styles.avatarImage} /> :
+            <Icon name={EIcon.Anon} />
+        }
+      </div>
+      <div className={styles.username}>
+        <Text size={20} color={props.username ? EColor.black : EColor.gray99}>{props.username || "Аноним" }</Text>
+      </div>
+    </a>
+  );
+}
